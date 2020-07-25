dadf5258d62ae1eeea537729d5dd17150486be63
Uduak Essien <acushla4real@gmail.com>
2020-07-25 09:46:32 +0100

    Add sprite images, bootstrap navbar and move it to partial _navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,8 +5,15 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
+require("jquery3").start()
+require("popper").start()
+require("bootstrap-sprockets").start()
 require("@rails/activestorage").start()
 require("channels")
+
+// require jquery3
+// require popper
+// require bootstrap-sprockets
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
