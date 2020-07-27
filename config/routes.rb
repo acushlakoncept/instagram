f8c36d09a27befd6dce297026150fa449d792b8d
Uduak Essien <acushla4real@gmail.com>
2020-07-28 00:45:44 +0100

    add users_controllers and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/users/:id' to 'users#show'
+  resource :users, only: [:show]
 end
