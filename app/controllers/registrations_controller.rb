06ec0dec7366873c5e3a044aafbdd1f8e2aa3966
Uduak Essien <acushla4real@gmail.com>
2020-07-29 04:27:44 +0100

    Update the UI of the edit profile page

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,8 @@
+class RegistrationsController < Devise::RegistrationsController
+
+    protected
+
+    def update_resources(resource, params)
+        resource.update_without_password(params)
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users,
     path: '',
     path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
-    controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+    controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # get '/users/:id' to 'users#show'
