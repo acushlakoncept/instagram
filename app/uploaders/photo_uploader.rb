eb6a38200b7c9bc144dd1a913dffeabff3a62d8c
Uduak Essien <acushla4real@gmail.com>
2020-07-30 02:35:09 +0100

    Add cloudinary gem and created cloundinary.rb with config files in initializer folder, add cloudinary config for carrierwave in photo_uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,13 +4,26 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  include Cloudinary::CarrierWave
+
+  process :convert => 'png'
+  process :tags => ['post_picture']
+
+  version :standard do
+    process :resize_to_fill => [300, 300, :north]
+  end
+
+  version :thumbnail do
+    resize_to_fit(100, 100)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+    config.cloud.name = "acushlakoncepts"
+    config.api_key = "646433418878379"
+    config.api_secret = "VQ-BqLg3LNpMwIaTynKAM6I9xVc"
+    config.cdn_subdomain = true
+end
